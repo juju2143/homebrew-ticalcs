ffb63f79b0c9d7dca7bf223f0fca03ab513fc370
Adrien Bertrand <bertrand.adrien@gmail.com>
2018-10-12 13:20:50 +0800

    Use cleaner link for TI-Connect
    I... guess this one is better?
    As you want ¯\_(ツ)_/¯

--- Casks/ti-connect.rb
+++ Casks/ti-connect.rb
@@ -2,7 +2,7 @@ cask 'ti-connect' do
   version '4.0'
   sha256 '4404f04464ba977b81227d6546a82868c7f58d2ebbda5b116e613d884d4c5cb8'
 
-  url "https://education.ti.com/download/en/ed-tech/D7445DBBA5124FA9B4F3F1B222A8005A/54FDF69C5FAB4143B20C7CC779FA51BD/TIConnect_#{version.no_dots}.dmg"
+  url "https://education.ti.com/en/-/media/files/download-center/software/computer_software/ti-connect/ticonnect_#{version.no_dots}.dmg"
   name 'TI Connect™'
   homepage 'https://education.ti.com/en/products/computer-software/ti-connect-sw'
 
